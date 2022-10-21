eee2d0bb3b7820ac3e599f0bd20642ccb69156b6
Michelly <michelly.daiana@hotmail.com>
2022-10-21 17:21:07 -0300

    requirement 7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <p>Sistema Solar</p>
+        <Title />
         <SolarSystem />
+        <Missions />
       </>
     );
   }
---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    return (
+      <h2> </h2>
+    );
+  }
+}
+
+export default Title;
