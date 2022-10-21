e58f7b549bdccb27fc4ae242f5034a2d292e0927
Michelly <michelly.daiana@hotmail.com>
2022-10-21 19:08:47 -0300

    requirement 4

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
+    // const { headline } = 'Planetas';
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
     );
   }
 }
