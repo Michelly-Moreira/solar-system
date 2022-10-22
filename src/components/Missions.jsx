5417368896dc586336eb4a4f34f764e7f3c823f3
Michelly <michelly.daiana@hotmail.com>
2022-10-22 18:22:41 -0300

    validation prop

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions" />
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
+
+Missions.propTypes = {
+  headline: PropTypes.string,
+}.isRequired;
+
+// renderizando o componente Title dentro do componente Missions, recebendo a prop headline e dando o valor "Missões" á prop.
 
 export default Missions;
